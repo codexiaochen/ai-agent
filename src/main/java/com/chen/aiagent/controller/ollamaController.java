61a1f300bd20286ea460144207ca1f7fcd791d06
abc <1163827926@qq.com>
2025-04-29 14:01:44 +0800

    接入本地部署的模型

--- src/main/java/com/chen/aiagent/controller/ollamaController.java
+++ src/main/java/com/chen/aiagent/controller/ollamaController.java
@@ -1,4 +1,24 @@
 package com.chen.aiagent.controller;
 
+import jakarta.annotation.Resource;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/chat")
 public class ollamaController {
+
+    @Resource
+    private ChatModel deepseekR1ChatModel;
+
+    @PostMapping("/ollama")
+    public String getMessage(String message){
+        ChatResponse response = deepseekR1ChatModel.call(new Prompt(message));
+        String result = response.getResult().getOutput().getText();
+        return result;
+    }
 }
